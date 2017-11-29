288a7ecdfab120e9df3125754fe85c91654ed255
pbaptiste64 <pmbaptiste64@gmail.com>
2017-11-29 18:15:14 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,37 @@
+/*console.log("Ready for Javascript")*/
+
+var oper1 = 24 + 0
+var oper2 = 24 - 0
+var oper3 = 24 / 1
+var oper4 = 24 * 1
+
+// console.log(oper3)
+
+
+var op1 = 6 % 3
+var op2 = 10 % 2
+var op3 = 5 % 2
+
+// console.log(op1==op2)
+// console.log(op1!==op2)
+
+var string1 = "Test Grade as Follows"
+var string2 = "Student1: 96"
+var string3 = "Comment: \"None\""
+
+// console.log(string1+"\n \t"+string2+"\n \t"+string3)
+
+var numbers = [0,10,20,30,40,50,60,70,80,90,100]
+
+// console.log(numbers[2+4])
+
+var myself ={
+    first: "Phil",
+    last: "Baptiste",
+    age: "24",
+    food: "pasta",
+    sport: "basketball",    
+}
+
+console.log("Hi my name is "+myself.first+" "+ myself.last)
+
